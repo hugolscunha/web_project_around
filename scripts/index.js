8f7fd157c6b78e7a93729b026684f1b947bffc91
hugolscunha <hugoleonardoscunha@gmail.com>
2025-10-06 15:20:06 -0300

    added popups and stylized popups

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,6 @@
-// Variaveis
-let editProfileButton = document.querySelector('.profile__edit-icon');
-let editProfileModal = document.querySelector('.modal');
-let closeModalButton = document.querySelector('.modal__close-button');
-let formElement = document.querySelector('.modal__form');
-let profileNameInput = document.querySelector('#profile-name');
-let profileDescriptionInput = document.querySelector('#profile-description');
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-
-// Preenchimento de Formulário
-function hanldeProfileFormSubmit (evt) {
-    evt.preventDefault();
-    console.log('formulário enviado')
-    profileName.textContent = profileNameInput.value;
-    profileDescription.textContent = profileDescriptionInput.value;
-    closeModal();
-}
-
-// Open modal function
-function openModal() {
-    editProfileModal.classList.add('modal__opened'); // Adiciona a classe para mostrar o popup
-    profileNameInput.value = profileName.textContent.trim();
-    profileDescriptionInput.value = profileDescription.textContent.trim();
-}
-
-// Close modalfuntion
-function closeModal() {
-    editProfileModal.classList.remove('modal__opened'); // Remove a classe para fechar o popup
-}
-
-// Event listeners
-editProfileButton.addEventListener('click', openModal);
-closeModalButton.addEventListener('click', closeModal);
-formElement.addEventListener('submit', hanldeProfileFormSubmit);
-
+//Seleciuona o botão de edição e o popup d e perfil
+const editButton = document.querySelector("#edit-button"); //Botão para abrir popup de dição
+const editPopup = document.querySelector("")
 
 
 
